0fcefa55847f709821c34496634092a03264c667
hashiguchip <hashiguchip@gmail.com>
2019-06-01 16:03:27 +0900

    [add] nuxt js

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Welcome!</h1>
+    <div>これはテストページです</div>
+    <NLink to="/about">
+      About Page
+    </NLink>
+  </div>
+</template>
+
+<script>
+export default {
+  head: {
+    title: "Home page"
+  }
+};
+</script>
